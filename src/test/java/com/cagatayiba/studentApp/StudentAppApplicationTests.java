7807e54f3f99bb9eecea80813697a00872188040
cagatay iba <cagatay.iba@gmail.com>
2022-11-26 20:53:46 +0300

    first commit

---
+++ src/test/java/com/cagatayiba/studentApp/StudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagatayiba.studentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
